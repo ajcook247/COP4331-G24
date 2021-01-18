b2f89c0808db41760cf77048730051f9bd91cfc1
Matthew Gomez <50896401+MGo01@users.noreply.github.com>
2021-01-18 10:16:05 -0500

    Add files via upload
    Added icons and a "show password" checkbox.

---
+++ app.js
@@ -0,0 +1,12 @@
+// Anna Malaj, Matthew Gomez
+// COP 4331, Spring 2021
+// 1/17/2021
+
+function showPassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
