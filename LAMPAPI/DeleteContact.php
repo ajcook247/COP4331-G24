aad876be732fbab06db13d22670069d86a29d3fe
root <ajc14654@knights.ucf.edu>
2021-01-19 04:52:17 +0000

    test

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,49 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	
+	$firstName = "";
+    $lastName = "";
+    $userName = "";
+    $password = "";
+    
+
+	$conn = new mysqli("localhost", "DatabaseAdmin", "COP4331isVeryFun", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        $sql = "DELETE FROM Contacts WHERE UserID= '" . $inData["UserID"] . "'";
+
+        if( $result = $conn->query($sql) != TRUE )
+        {
+            returnWithError( $conn->error );
+        }
+        
+        $conn->close();
+	
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	
+	
+?>
